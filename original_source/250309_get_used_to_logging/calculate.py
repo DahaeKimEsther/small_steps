dbcaa099175873adaaa7329c99bd9b50e79a83bf
DahaeKimEsther <vvidream7125@gmail.com>
2025-03-19 14:57:00 +0900

    get_used_to_logging - Edit formatter not to use logger in every py files

--- original_source/250309_get_used_to_logging/calculate.py
+++ original_source/250309_get_used_to_logging/calculate.py
@@ -1,30 +1,28 @@
 import logging
 import logging.config
 logging.config.fileConfig("C:/Users/dahae/workspace/projects/small_steps/original_source/250309_get_used_to_logging/logger.conf")
-logger = logging.getLogger(__name__)
-logger.info('set calculation.py')
+logging.info('set calculation.py')
 
 class calculation:
     def __init__(self):
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.logger.info(f"creating an instance of {__name__}.{self.__class__.__name__}")
+        logging.info(f"creating an instance of {__name__}.{self.__class__.__name__}")
     
     def sum(self, a:int, b:int):
-        self.logger.info("executing: sum()")
+        logging.info("executing: sum()")
         return a + b
     
     def substract(self, a:int, b:int):
-        self.logger.info("executing: substract()")
+        logging.info("executing: substract()")
         return a - b
     
     def multiply(self, a:int, b:int):
-        self.logger.info("executing: multiply()")
+        logging.info("executing: multiply()")
         return a * b
     
     def divide(self, a:int, b:int):
-        self.logger.info("executing: divide()")
+        logging.info("executing: divide()")
         try:
             result = a / b
             return result
         except Exception as e:
-            self.logger.error(f"getting error in divide() - {e}", exc_info=True)
+            logging.error(f"getting error in divide() - {e}", exc_info=True)
--- original_source/250309_get_used_to_logging/main.py
+++ original_source/250309_get_used_to_logging/main.py
@@ -4,30 +4,15 @@ import logging
 import logging.config
 import calculate
 logging.config.fileConfig('C:/Users/dahae/workspace/projects/small_steps/original_source/250309_get_used_to_logging/logger.conf')
-logger = logging.getLogger(__name__)
-
-# #logging.config 없을 경우
-# #main 로거
-# main_logger = logging.getLogger('main')
-# main_logger.setLevel(logging.DEBUG)
-
-# # Handler에 formatter 포함
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# ch.setFormatter(formatter)
-
-# # handler 적용
-# main_logger.addHandler(ch)
 
 # logger 사용
-logger.info("creating an instance of calculation.calculation, called 'calc'")
+logging.info("creating an instance of calculation.calculation, called 'calc'")
 calc = calculate.calculation()
 
-logger.info('setting a,b for calc.divide()')
+logging.info('setting a,b for calc.divide()')
 a = 10
 b = 0
 
-logger.info('executing calc.divide()')
+logging.info('executing calc.divide()')
 result = calc.divide(a, b)
-logger.info(f"getting result of cal.divide: {a} / {b} = {result}")
+logging.info(f"getting result of cal.divide: {a} / {b} = {result}")
